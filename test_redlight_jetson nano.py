8cb80bc3200d67b08e95b4a564f47b3af7cfc7f9
coco <m842953@gmail.com>
2022-03-02 12:27:58 +0800

    add test redlight

--- test_redlight_jetson nano.py
+++ test_redlight_jetson nano.py
@@ -1,32 +1,30 @@
-import RPi.GPIO as GPIO
+import board
+import digitalio
+import busio
+
+
 import time
 
 
-# 設定PIN腳控制模式
-GPIO.setmode(GPIO.BOARD)
-# 設定要操作的PIN腳
-input_pin = 18
-# 設定要操作的PIN腳為輸入還是輸出
-GPIO.setup(input_pin, GPIO.IN)
-print("Set redl 18 as input!" )
 
-GPIO.setwarnings(False)
+D24 = digitalio.DigitalInOut(board.D24)
+D24.direction = digitalio.Direction.INPUT
 
 i = 0 # 計算秒數
 t = 0.5
 try:
     while True:
         time.sleep(t)
-        value = GPIO.input(input_pin)
+        value = D24.value
 
         #if value == GPIO.HIGH:
         #    print("HIGH，偵測範圍內沒人")
         #elif value == GPIO.LOW:
         #    print("LOW，有人在偵測範圍內")
-        if value == GPIO.LOW:
+        if value == True:
             i = i + t
             print(f"{i:.2f}","LOW，偵測範圍內沒人")
-        elif value == GPIO.HIGH:
+        elif value == False:
             i = i + t
             print(f"{i:.2f}","HIGH，有人在偵測範圍內")
 finally:
